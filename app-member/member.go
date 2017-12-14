f29ab14540e30eaf1ca10866228cc7e17f3b4c6b
Jarlyyn <jarlyyn@qq.com>
2017-12-14 22:41:23 +0800

    update

--- app-member/member.go
+++ app-member/member.go
@@ -2,7 +2,6 @@ package member
 
 import (
 	"errors"
-	"reflect"
 
 	"github.com/herb-go/herb/cache"
 	"github.com/herb-go/herb/cache-session"
@@ -12,11 +11,9 @@ import (
 const prefixCacheMember = "M"
 const prefixCacheAccount = "A"
 const prefixCacheData = "D"
+const prefixCacheCollection = "C"
 
-var ErrDataServiceNotFound = errors.New("error data service not found")
-var ErrDataMustBePtr = errors.New("error must post a pointer to get data")
-var ErrDataNil = errors.New("error null data")
-var ErrDataTypeNotMatch = errors.New("user data type not match")
+var ErrProfileServiceNotFound = errors.New("error data service not found")
 
 type Member struct {
 	UID     string
@@ -26,27 +23,27 @@ type Member struct {
 type MemberService struct {
 	Has                func(uid string) (bool, error)
 	CheckAvailablefunc func(uid string) (bool, error)
-	Member             func(uid string) (Member, error)
+	Member             func(uid ...string) ([]Member, error)
 }
 type AccountsService struct {
-	Accounts     func(uid string) ([]user.UserAccount, error)
+	Accounts     func(uid ...string) ([][]user.UserAccount, error)
 	AccountToUID func(user.UserAccount) (string, error)
 }
-type DataService func(uid string) (v interface{}, err error)
+type ProfileService func(uid ...string) (profile []user.Profile, err error)
 type Service struct {
 	UIDField        *session.Field
 	ContentName     string
 	Cache           cache.Cacheable
 	MemberService   *MemberService
 	AccountsService *AccountsService
-	DataServices    map[string]DataService
+	ProfileServices map[string]ProfileService
 }
 
 func (s *Service) MemberCache() cache.Cacheable {
 	return cache.NewNode(s.Cache, prefixCacheMember)
 }
 
-func (s *Service) CacheMember(m Member) error {
+func (s *Service) cacheMember(m Member) error {
 	return s.MemberCache().Set(m.UID, m, cache.DefualtTTL)
 }
 func (s *Service) CleanUserMemberCache(uid string) error {
@@ -56,9 +53,10 @@ func (s *Service) GetUserMember(uid string) (*Member, error) {
 	var member = Member{}
 	err := s.AccountsCache().Get(uid, &member)
 	if err == cache.ErrNotFound {
-		member, err := s.MemberService.Member(uid)
+		members, err := s.MemberService.Member(uid)
 		if err == nil {
-			s.CacheMember(member)
+			member = members[0]
+			s.cacheMember(member)
 		}
 	}
 	return &member, err
@@ -66,7 +64,7 @@ func (s *Service) GetUserMember(uid string) (*Member, error) {
 func (s *Service) AccountsCache() cache.Cacheable {
 	return cache.NewNode(s.Cache, prefixCacheAccount)
 }
-func (s *Service) CacheUserAccounts(uid string, accounts ...user.UserAccount) error {
+func (s *Service) cacheUserAccounts(uid string, accounts ...user.UserAccount) error {
 	return s.AccountsCache().Set(uid, accounts, cache.DefualtTTL)
 }
 func (s *Service) CleanUserAccountsCache(uid string) error {
@@ -76,65 +74,134 @@ func (s *Service) GetUserAccounts(uid string) ([]user.UserAccount, error) {
 	var accounts = []user.UserAccount{}
 	err := s.AccountsCache().Get(uid, &accounts)
 	if err == cache.ErrNotFound {
-		accounts, err := s.AccountsService.Accounts(uid)
+		accountsList, err := s.AccountsService.Accounts(uid)
 		if err != nil {
-			s.CacheUserAccounts(uid, accounts...)
+			accounts = accountsList[0]
+			s.cacheUserAccounts(uid, accounts...)
 		}
 	}
 	return accounts, err
 }
-func (s *Service) DataCache(field string) cache.Cacheable {
+func (s *Service) ProfileCache(field string) cache.Cacheable {
 	c := cache.NewNode(s.Cache, prefixCacheData)
 	return cache.NewNode(c, field)
 }
-func (s *Service) CacheData(field string, uid string, v interface{}) error {
+func (s *Service) cacheProfile(field string, uid string, v interface{}) error {
 	c := cache.NewNode(s.Cache, prefixCacheData)
 	return cache.NewNode(c, field).Set(uid, v, 0)
 }
-func (s *Service) CleanDataCache(field string, uid string) error {
+func (s *Service) CleanProfileCache(field string, uid string) error {
 	c := cache.NewNode(s.Cache, prefixCacheData)
 	return cache.NewNode(c, field).Del(uid)
 }
-func (s *Service) Data(field string, uid string, v interface{}) (err error) {
-	var data interface{}
-	var c = s.DataCache(field)
-	err = c.Get(uid, v)
-	if err == cache.ErrNotFound {
-		ds, ok := s.DataServices[field]
-		if ok == false {
-			return ErrDataServiceNotFound
-		}
-		data, err = ds(uid)
-		if err != nil {
-			return err
-		}
-		err = cp(v, data)
-		if err != nil {
-			return err
-		}
-		err = c.Set(uid, v, cache.DefualtTTL)
-	}
-	return err
+
+func (s *Service) UserCollection(uid string) *cache.Collection {
+	c := cache.NewNode(s.Cache, prefixCacheData)
+	return cache.NewCollection(c, uid, 0)
 }
 
-func cp(v interface{}, data interface{}) (err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			err = r.(error)
+func (s *Service) FlushUser(uid string) error {
+	err := s.MemberCache().Del(uid)
+	if err != nil {
+		return err
+	}
+	s.AccountsCache().Del(uid)
+	if err != nil {
+		return err
+	}
+	for k, _ := range s.ProfileServices {
+		err = s.ProfileCache(k).Del(uid)
+		if err != nil {
+			return err
 		}
-	}()
-	var vv = reflect.ValueOf(v)
-	if vv.Kind() != reflect.Ptr {
-		return ErrDataMustBePtr
 	}
-	if data == nil {
-		return ErrDataNil
+	err = s.UserCollection(uid).Flush()
+	return err
+}
+func (s *Service) Profile(field string, uid string) (profile user.Profile, err error) {
+	profile = user.Profile{}
+	var c = s.ProfileCache(field)
+	err = c.Get(uid, &profile)
+	if err == cache.ErrNotFound {
+		v, ok := s.ProfileServices[field]
+		if ok == false {
+			return nil, ErrProfileServiceNotFound
+		}
+		profileList, err := v(uid)
+		if err != nil {
+			return nil, err
+		}
+		profile = profileList[0]
+		err = c.Set(uid, v, cache.DefualtTTL)
 	}
-	var d = reflect.Indirect(reflect.ValueOf(data))
-	if vv.Elem().Type() != d.Type() {
-		return ErrDataTypeNotMatch
-	}
-	vv.Elem().Set(d)
-	return nil
+	return profile, err
+}
 
+type UserResult struct {
+	Member   Member
+	Accounts []user.UserAccount
+	Profiles map[string]user.Profile
+	service  *Service
+}
+
+func newUserResult(service *Service) *UserResult {
+	return &UserResult{
+		Member:   Member{},
+		Accounts: []user.UserAccount{},
+		Profiles: map[string]user.Profile{},
+		service:  service,
+	}
+}
+func (s *Service) ListUser(uid []string, withAccounts bool, fields []string) (map[string]UserResult, error) {
+	var err error
+	result := map[string]UserResult{}
+	uncachedMembers := []string{}
+	uncachedAccounts := []string{}
+	uncachedProfile := map[string][]string{}
+
+	for _, v := range uid {
+		if _, ok := result[v]; ok == true {
+			continue
+		}
+		userresult := UserResult{
+			Member:   Member{},
+			Accounts: []user.UserAccount{},
+			Profiles: map[string]user.Profile{},
+		}
+		err := s.MemberCache().Get(v, &(userresult.Member))
+		if err == cache.ErrNotFound {
+			err = nil
+			uncachedMembers = append(uncachedMembers, v)
+		}
+		if err != nil {
+			return nil, err
+		}
+		if withAccounts {
+			err = s.AccountsCache().Get(v, &(userresult.Accounts))
+			if err == cache.ErrNotFound {
+				err = nil
+				uncachedAccounts = append(uncachedAccounts, v)
+			}
+			if err != nil {
+				return nil, err
+			}
+		}
+		for _, field := range fields {
+			profile := user.Profile{}
+			if _, ok := userresult.Profiles[field]; ok == false {
+				userresult.Profiles[field] = profile
+			}
+			err = s.ProfileCache(field).Get(v, &(profile))
+			if err == cache.ErrNotFound {
+				err = nil
+				if _, ok := uncachedProfile[field]; ok == false {
+					uncachedProfile[field] = []string{}
+				}
+				uncachedProfile[field] = append(uncachedProfile[field], v)
+			}
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
 }
---
+++ middleware-zip/zip.go
@@ -0,0 +1,47 @@
+package zip
+
+import (
+	"compress/gzip"
+	"net/http"
+)
+
+func Middleware(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	w.Header().Set("Content-Encoding", "gzip")
+	next(&zipWriter{w, http.Header{}, 0}, r)
+}
+
+type zipWriter struct {
+	http.ResponseWriter
+	header http.Header
+	status int
+}
+
+func (w *zipWriter) Write(p []byte) (int, error) {
+	if w.status == 0 {
+		w.WriteHeader(200)
+	}
+	var writer, err = gzip.NewWriterLevel(w.ResponseWriter, gzip.BestCompression)
+	if err != nil {
+		return 0, err
+	}
+	defer writer.Close()
+	return writer.Write(p)
+}
+
+func (w *zipWriter) Header() http.Header {
+	return w.header
+}
+
+func (w *zipWriter) WriteHeader(status int) {
+	if w.status != 0 {
+		return
+	}
+	var header = w.ResponseWriter.Header()
+	for k, v := range w.header {
+		for _, val := range v {
+			header.Add(k, val)
+		}
+	}
+	w.status = status
+	w.ResponseWriter.WriteHeader(status)
+}
