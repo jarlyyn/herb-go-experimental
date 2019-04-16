ddd8422e3958de96481197943e95e4a093b568d0
jarlyyn <jarlyyn@qq.com>
2019-04-16 23:09:34 +0800

    update

--- updater/update.sh
+++ updater/update.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-go get -d github.com/herb-go/herb/cache
-go get -d github.com/herb-go/providers/sql/sqlcache
-go get -d github.com/herb-go/member
-go get -d github.com/herb-go/connections
-go get -d github.com/herb-go/externalauth
-go get -d github.com/herb-go/misc/debounce
-go get -d github.com/herb-go/fetch
-go get -d github.com/herb-go/herbgo
+go get -u -d github.com/herb-go/herb
+go get -u -d github.com/herb-go/providers
+go get -u -d github.com/herb-go/member
+go get -u -d github.com/herb-go/connections
+go get -u -d github.com/herb-go/externalauth
+go get -u -d github.com/herb-go/misc
+go get -u -d github.com/herb-go/fetch
+go get -u -d github.com/herb-go/herbgo
+go install github.com/herb-go/herbgo
 
