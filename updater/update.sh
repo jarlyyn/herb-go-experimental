7448e1cb8467ecc1b3e62afc88498f7b98fa3422
jarlyyn <jarlyyn@qq.com>
2019-04-18 23:21:18 +0800

    update

--- updater/update.sh
+++ updater/update.sh
@@ -6,6 +6,7 @@ go get -u -d github.com/herb-go/connections
 go get -u -d github.com/herb-go/externalauth
 go get -u -d github.com/herb-go/misc
 go get -u -d github.com/herb-go/fetch
+go get -u -d github.com/herb-go/notification
 go get -u -d github.com/herb-go/herbgo
 go install github.com/herb-go/herbgo
 
