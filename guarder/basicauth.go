32f7eca3e67438c2754eebc98c3c4760b62fe66b
Jarlyyn <jarlyyn@qq.com>
2019-11-09 01:16:45 +0800

    update

---
+++ guarder/basicauth.go
@@ -0,0 +1,42 @@
+package guarder
+
+import "net/http"
+
+type BasicAuth struct {
+}
+
+func NewBasicAuth() *BasicAuth {
+	return &BasicAuth{}
+}
+func (h *BasicAuth) ReadParamsFromRequest(r *http.Request) (*Params, error) {
+	p := NewParams()
+	id, token, ok := r.BasicAuth()
+	if !ok {
+		return p, nil
+	}
+	p.SetID(id)
+	p.SetToken(token)
+	return p, nil
+}
+func (h *BasicAuth) WriteParamsToRequest(r *http.Request, p *Params) error {
+	r.SetBasicAuth(p.ID(), p.Token())
+	return nil
+}
+
+func createBasicAuthWithConfig(conf Config, prefix string) (*BasicAuth, error) {
+	var err error
+	v := NewBasicAuth()
+	return v, err
+}
+
+func basicAuthMapperFactory(conf Config, prefix string) (Mapper, error) {
+	return createBasicAuthWithConfig(conf, prefix)
+}
+
+func registerBasicAuthFactories() {
+	RegisterMapper("basicauth", basicAuthMapperFactory)
+}
+
+func init() {
+	registerBasicAuthFactories()
+}
