1855d0cf9a27e88302e336cec50f5427e1686f7b
Jarlyyn <jarlyyn@qq.com>
2019-11-24 23:35:32 +0800

    update

--- assembler/type.go
+++ assembler/type.go
@@ -9,9 +9,9 @@ type CommonType interface{}
 var TypeBool = CommonType(reflect.TypeOf(false))
 var TypeString = CommonType(reflect.TypeOf(""))
 var TypeInt = CommonType(reflect.TypeOf(int(0)))
-var TypeUInt = CommonType(reflect.TypeOf(uint(0)))
+var TypeUint = CommonType(reflect.TypeOf(uint(0)))
 var TypeInt64 = CommonType(reflect.TypeOf(int64(0)))
-var TypeUInt64 = CommonType(reflect.TypeOf(int64(0)))
+var TypeUint64 = CommonType(reflect.TypeOf(int64(0)))
 
 var TypeInterface = CommonType(reflect.TypeOf((*interface{})(nil)).Elem())
 var TypeMap = CommonType(reflect.TypeOf(map[interface{}]interface{}{}))
--- assembler/typechecker.go
+++ assembler/typechecker.go
@@ -1,6 +1,75 @@
 package assembler
 
+import "reflect"
+
 type TypeChecker struct {
 	Type      interface{}
 	CheckType func(a *Assembler) (bool, error)
+}
+
+func getReflectType(v interface{}) reflect.Type {
+	return reflect.TypeOf(reflect.Indirect(reflect.ValueOf(v)))
+}
+
+var TypeCheckerString = &TypeChecker{
+	Type: TypeString,
+	CheckType: func(a *Assembler) (bool, error) {
+		v, err := a.Value()
+		if err != nil {
+			return false, err
+		}
+		return getReflectType(v).Kind() == reflect.String, nil
+	},
+}
+
+var TypeCheckerBool = &TypeChecker{
+	Type: TypeBool,
+	CheckType: func(a *Assembler) (bool, error) {
+		v, err := a.Value()
+		if err != nil {
+			return false, err
+		}
+		return getReflectType(v).Kind() == reflect.Bool, nil
+	},
+}
+
+var TypeCheckerInt = &TypeChecker{
+	Type: TypeInt,
+	CheckType: func(a *Assembler) (bool, error) {
+		v, err := a.Value()
+		if err != nil {
+			return false, err
+		}
+		return getReflectType(v).Kind() == reflect.Int, nil
+	},
+}
+var TypeCheckerUiit = &TypeChecker{
+	Type: TypeUint,
+	CheckType: func(a *Assembler) (bool, error) {
+		v, err := a.Value()
+		if err != nil {
+			return false, err
+		}
+		return getReflectType(v).Kind() == reflect.Uint, nil
+	},
+}
+var TypeCheckerInt64 = &TypeChecker{
+	Type: TypeInt64,
+	CheckType: func(a *Assembler) (bool, error) {
+		v, err := a.Value()
+		if err != nil {
+			return false, err
+		}
+		return getReflectType(v).Kind() == reflect.Int64, nil
+	},
+}
+var TypeCheckerUint64 = &TypeChecker{
+	Type: TypeUint64,
+	CheckType: func(a *Assembler) (bool, error) {
+		v, err := a.Value()
+		if err != nil {
+			return false, err
+		}
+		return getReflectType(v).Kind() == reflect.Uint64, nil
+	},
 }
