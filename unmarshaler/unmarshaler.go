c45d4c9d7f1b66519a823db911126f392249a843
jarlyyn <jarlyyn@qq.com>
2019-11-29 00:02:41 +0800

    update

--- unmarshaler/unmarshaler.go
+++ unmarshaler/unmarshaler.go
@@ -3,3 +3,16 @@ package unmarshaler
 type Unmarshaler interface {
 	Unmarshal(data []byte, v interface{}) error
 }
+
+type DataConverter struct {
+	marshaler   func(v interface{}) ([]byte, error)
+	unmarshaler func(data []byte, v interface{}) error
+}
+
+func (c *DataConverter) Marshal(v interface{}) ([]byte, error) {
+	return c.marshaler(v)
+}
+
+func (c *DataConverter) Unmarshal(data []byte, v interface{}) error {
+	return c.unmarshaler(data, v)
+}
