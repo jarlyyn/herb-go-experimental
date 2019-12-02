c04a0aeab59f4f75ff05b18560043fb38b988e98
jarlyyn <jarlyyn@qq.com>
2019-12-02 22:45:34 +0800

    update

--- unmarshaler/lazyload.go
+++ unmarshaler/lazyload.go
@@ -1,20 +1,28 @@
 package unmarshaler
 
+//AssemblerLazyLoader assembler lazy loader struct
 type AssemblerLazyLoader struct {
 	Assembler *Assembler
 }
 
+//LazyLoad lazeload data into interface.
+//Return any error if raised
 func (l *AssemblerLazyLoader) LazyLoad(v interface{}) error {
 	_, err := l.Assembler.Assemble(v)
 	return err
 }
 
+//NewLazyLoader create new assembler lazy loader
 func NewLazyLoader() *AssemblerLazyLoader {
 	return &AssemblerLazyLoader{}
 }
 
+//LazyLoaderFunc lazy loader func interface
 type LazyLoaderFunc func(v interface{}) error
 
+//LazyLoader lazy loader interface
 type LazyLoader interface {
+	//LazyLoad lazeload data into interface.
+	//Return any error if raised
 	LazyLoad(v interface{}) error
 }
