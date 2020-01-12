d603e6681d1b5d3b448eb6dfa46c8eb240d8debe
jarlyyn <jarlyyn@qq.com>
2020-01-12 23:55:36 +0800

    update

---
+++ pattern/context.go
@@ -0,0 +1,7 @@
+package pattern
+
+type ContextKey string
+
+var ContextKeyIPAddress = ContextKey("ipaddr")
+
+var ContextKeyIP = ContextKey("ip")
--- pattern/ipnets.go
+++ pattern/ipnets.go
@@ -1,15 +1,33 @@
 package pattern
 
 import (
+	"context"
 	"net"
 	"net/http"
 )
 
 func GetRequestIPAddress(r *http.Request) string {
+	v := r.Context().Value(ContextKeyIPAddress)
+	if v != nil {
+		return v.(string)
+	}
+	ip, _, _ := net.SplitHostPort(r.RemoteAddr)
+	ctx := context.WithValue(r.Context(), ContextKeyIPAddress, ip)
+	req := r.WithContext(ctx)
+	*r = *req
+	return ip
 
 }
 func GetRequestIP(r *http.Request) net.IP {
-
+	v := r.Context().Value(ContextKeyIP)
+	if v != nil {
+		return v.(net.IP)
+	}
+	ip := net.ParseIP(GetRequestIPAddress(r))
+	ctx := context.WithValue(r.Context(), ContextKeyIP, ip)
+	req := r.WithContext(ctx)
+	*r = *req
+	return ip
 }
 
 type IPNets []*net.IPNet
