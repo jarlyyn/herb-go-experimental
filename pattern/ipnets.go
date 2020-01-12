4cc9669bcea23eb90fa2673c498f87d0bd1b6283
jarlyyn <jarlyyn@qq.com>
2020-01-12 23:49:46 +0800

    update

--- pattern/ipnets.go
+++ pattern/ipnets.go
@@ -2,6 +2,34 @@ package pattern
 
 import (
 	"net"
+	"net/http"
 )
 
+func GetRequestIPAddress(r *http.Request) string {
+
+}
+func GetRequestIP(r *http.Request) net.IP {
+
+}
+
 type IPNets []*net.IPNet
+
+func (i IPNets) IsEmpty() bool {
+	return len(i) == 0
+}
+
+func (i IPNets) Match(r *http.Request) (bool, error) {
+	if i.IsEmpty() {
+		return false, nil
+	}
+	ip := GetRequestIP(r)
+	if ip == nil {
+		return false, nil
+	}
+	for k := range i {
+		if i[k].Contains(ip) {
+			return true, nil
+		}
+	}
+	return false, nil
+}
--- pattern/urls.go
+++ pattern/urls.go
@@ -1,1 +1,9 @@
 package pattern
+
+type FixedURLs map[string]bool
+
+type FixedURLWithHost map[string]FixedURLs
+
+type URLPrefixs []string
+
+type URLPrefixsWithHost map[string]URLPrefixs
