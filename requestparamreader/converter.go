a84bf2f2fbe430363b63c90de8fcd9a32f8ba97a
jarlyyn <jarlyyn@qq.com>
2020-06-28 21:10:10 +0800

    update

--- requestparamreader/converter.go
+++
@@ -1,9 +0,0 @@
-package requestparamreader
-
-type Converter interface {
-	ConvertParam([]byte) ([]byte, bool, error)
-}
-
-type ConverterFactory interface {
-	CreateConverter(loader func(interface{}) error) (Converter, error)
-}
