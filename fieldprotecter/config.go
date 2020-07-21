df1f86d1b4e43bf508cd8364cf4e48d2588afec9
jarlyyn <jarlyyn@qq.com>
2020-07-21 20:15:21 +0800

    update

--- fieldprotecter/config.go
+++ fieldprotecter/config.go
@@ -5,16 +5,17 @@ import (
 
 	"github.com/herb-go/herbsecurity/authority/credential"
 	"github.com/herb-go/httpinfomanager"
-	"github.com/herb-go/protect/protecter"
+	"github.com/herb-go/protecter"
 	actionoverseer "github.com/herb-go/providers/herb/overseers/actionoverseer"
+	authenticatorfactoryoverseer "github.com/herb-go/providers/herb/overseers/authenticatorfactoryoverseer"
 	"github.com/herb-go/worker"
 )
 
 type Config struct {
-	Fields         map[string]*httpinfomanager.FieldName
-	OnFailWorkerID string
-	Verifier       string
-	VerifierConfig func(v interface{}) error
+	Fields       map[string]*httpinfomanager.FieldName
+	OnFailAction string
+	AuthType     string
+	AuthConfig   func(v interface{}) error `config:", lazyload"`
 }
 
 func (c *Config) ApplyTo(p protecter.Protecter) error {
@@ -30,12 +31,21 @@ func (c *Config) ApplyTo(p protecter.Protecter) error {
 		})
 	}
 	p.Credentialers = credentialers
-	if c.OnFailWorkerID != "" {
-		a := actionoverseer.GetActionByID(c.OnFailWorkerID)
+	if c.OnFailAction != "" {
+		a := actionoverseer.GetActionByID(c.OnFailAction)
 		if a == nil {
-			return fmt.Errorf("%w (%s)", worker.ErrWorkerNotFound, c.OnFailWorkerID)
+			return fmt.Errorf("%w (%s)", worker.ErrWorkerNotFound, c.OnFailAction)
 		}
 		p.OnFail = a
 	}
+	authfactory := authenticatorfactoryoverseer.GetAuthenticatorFactoryByID(c.AuthType)
+	if authfactory == nil {
+		return fmt.Errorf("%w (%s)", worker.ErrWorkerNotFound, c.AuthType)
+	}
+	auth, err := authfactory.CreateAuthenticator(c.AuthConfig)
+	if err != nil {
+		return err
+	}
+	p.Authenticator = auth
 	return nil
 }
