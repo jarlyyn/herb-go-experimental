5dbc834b17f66e37833d32696412543e8a7973d6
jarlyyn <jarlyyn@qq.com>
2020-08-21 00:33:16 +0800

    update

--- updater/update.sh
+++ updater/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 go get -u -d github.com/herb-go/herb
 go get -u -d github.com/herb-go/providers
-go get -u -d github.com/herb-go/member
+go get -u -d github.com/herb-go/deprecated/member
 go get -u -d github.com/herb-go/connections
 go get -u -d github.com/herb-go/externalauth
 go get -u -d github.com/herb-go/misc
